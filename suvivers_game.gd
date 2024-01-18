d7da2936106867614296d60c7e8753258f52fc00
eklownr <eklownr@gmail.com>
2024-01-18 12:48:22 +0100

    ändrad:        .godot/editor/editor_layout.cfg 	ändrad:        .godot/editor/filesystem_cache8 	ändrad:        .godot/editor/filesystem_update4 	ändrad:        .godot/editor/mob.tscn-editstate-5b94865898bd9b5cb44e426bf42f6f48.cfg 	ändrad:        .godot/editor/pine_tree.tscn-editstate-bf993d67081309dd66a6fbfd837d4c3c.cfg 	ändrad:        .godot/editor/project_metadata.cfg 	ändrad:        .godot/editor/recent_dirs 	ändrad:        .godot/editor/script_editor_cache.cfg 	ändrad:        .godot/editor/suvivers_game.tscn-editstate-ec64355f2f59dfe14b5d681f8ae084b2.cfg 	ändrad:        .godot/editor/suvivers_game.tscn-folding-ec64355f2f59dfe14b5d681f8ae084b2.cfg 	ändrad:        .godot/exported/133200997/export-5b94865898bd9b5cb44e426bf42f6f48-mob.scn 	ändrad:        .godot/exported/133200997/export-6350fccf6e14d2f0c891730ee1d2c6cd-bullet.scn 	ändrad:        .godot/exported/133200997/export-b254bef953a17239ecd110db09321b87-gun.scn 	ändrad:        .godot/exported/133200997/export-bf993d67081309dd66a6fbfd837d4c3c-pine_tree.scn 	ändrad:        .godot/exported/133200997/export-ec64355f2f59dfe14b5d681f8ae084b2-suvivers_game.scn 	ändrad:        .godot/exported/133200997/file_cache 	ändrad:        .godot/uid_cache.bin 	ändrad:        export_presets.cfg 	ändrad:        item.gd 	ändrad:        mob.gd 	ändrad:        pine_tree.tscn 	ändrad:        suvivers_game.gd 	.godot/editor/export-8483fa81b63c99379aead3ad42088602-item.scn-folding-af5fe9613cf9b32d008e0fcd53e0d4ad.cfg 	.godot/editor/export-b2e26e438636898eddb9e1b54f81a9ee-heart.scn-folding-3c22ad06f91d6aabf307cb23ec5a05ed.cfg 	.godot/editor/pine_tree.gd-folding-ad8ede5c664117194814203c5bf4069e.cfg 	.godot/exported/133200997/export-8483fa81b63c99379aead3ad42088602-item.scn 	.godot/exported/133200997/export-b2e26e438636898eddb9e1b54f81a9ee-heart.scn 	pine_tree.gd 	web/

--- suvivers_game.gd
+++ suvivers_game.gd
@@ -6,30 +6,33 @@ extends Node2D
 func _ready():
 	%GameOver.visible = false
 
-
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
- 
+func spawn_tree():
+	var new_tree = preload("res://pine_tree.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_tree.global_position = %PathFollow2D.global_position
+	add_child(new_tree)
+	
 func _on_timer_timeout():
 	scoreLable.text = str(player.score)	
 	spawn_mob()
-
+	spawn_tree()
 
 func _on_player_health_depleted():
 	%GameOver.visible = true	
-	%player.health = 0
+	%player.health = 0	
 	#get_tree().paused = true
 	if Input.is_action_just_pressed("restart"):
 		# kill all mob-ememies
 		var children = get_children()
 		for child in children:	
-			if child.has_method("take_damage"):
-				for i in 6: 
-					child.take_damage()
+			if child.has_method("restart"):
+				child.restart()
 		%player.score = 0
 		%player.health = 100
 		%player/ProgressBar.value = 100
